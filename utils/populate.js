fb3fb89b297b9ba70528eef0296eb9900be005c3
Ivan Bespalov <slate245@gmail.com>
2020-02-02 12:57:06 +0500

    Created temporary db population utility

---
+++ utils/populate.js
@@ -0,0 +1,90 @@
+const { set } = require("date-fns");
+const { Schedule } = require("./models/schedule");
+
+function populateWorkingHours(date) {
+  const from = 8;
+  const to = 21;
+  const workingHours = [];
+  for (let i = from; i <= to; i++) {
+    workingHours.push(
+      set(date, { hours: i, minutes: 0, seconds: 0, milliseconds: 0 })
+    );
+  }
+  return workingHours;
+}
+
+export async function createSchedule() {
+  const date = new Date("January 7 2020");
+  const workingHours = populateWorkingHours(date);
+  const schedule = new Schedule({
+    date: date,
+    workingHours,
+    plannedActivities: [
+      {
+        name: "Первая встреча",
+        allocatedTimeslot: {
+          begining: set(date, {
+            hours: 9,
+            minutes: 0,
+            seconds: 0,
+            milliseconds: 0
+          }),
+          end: set(date, { hours: 9, minutes: 45, seconds: 0, milliseconds: 0 })
+        }
+      },
+      {
+        name: "Вторая встреча",
+        allocatedTimeslot: {
+          begining: set(date, {
+            hours: 10,
+            minutes: 15,
+            seconds: 0,
+            milliseconds: 0
+          }),
+          end: set(date, {
+            hours: 10,
+            minutes: 30,
+            seconds: 0,
+            milliseconds: 0
+          })
+        }
+      },
+      {
+        name: "Третья встреча",
+        allocatedTimeslot: {
+          begining: set(date, {
+            hours: 11,
+            minutes: 30,
+            seconds: 0,
+            milliseconds: 0
+          }),
+          end: set(date, {
+            hours: 12,
+            minutes: 45,
+            seconds: 0,
+            milliseconds: 0
+          })
+        }
+      },
+      {
+        name: "Четвертая встреча",
+        allocatedTimeslot: {
+          begining: set(date, {
+            hours: 14,
+            minutes: 45,
+            seconds: 0,
+            milliseconds: 0
+          }),
+          end: set(date, {
+            hours: 15,
+            minutes: 30,
+            seconds: 0,
+            milliseconds: 0
+          })
+        }
+      }
+    ]
+  });
+  const result = await schedule.save();
+  console.log(result);
+}
