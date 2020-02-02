b4e09f4358ba9c76ed09a78a322d8f0191bf07a5
Ivan Bespalov <slate245@gmail.com>
2020-02-02 13:26:24 +0500

    Set up connection to database

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 
 const app = express();
 
+require("./startup/db")();
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
---
+++ startup/db.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = function() {
+  mongoose
+    .connect(process.env.DB_STRING, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    .then(() => console.log(`Connected to ${process.env.DB_STRING}...`))
+    .catch(err => console.error("Could not connect to MongoDB...", err));
+};
