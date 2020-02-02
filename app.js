cf1766a79b4ef6a0d85e27362172ce4e56cd1af5
Ivan Bespalov <slate245@gmail.com>
2020-02-02 13:15:04 +0500

    Set up Express server and dotenv

---
+++ app.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () =>
+  console.log(`Listening on port ${port}...`)
+);
+
+module.exports = server;
