fb9a7977e2551ff964a6d9a6c6411248e29c1f73
Ivan Bespalov <slate245@gmail.com>
2020-02-04 15:34:39 +0500

    Changed local port

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 require("./startup/routes")(app);
 require("./startup/db")();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3900;
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 );
