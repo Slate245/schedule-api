2820662f8a3516a97a98c9842d33a750395f1492
Ivan Bespalov <slate245@gmail.com>
2020-02-24 18:21:38 +0500

    Added creation of new Schedules

--- models/schedule.js
+++ models/schedule.js
@@ -1,4 +1,22 @@
+const Joi = require("@hapi/joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+function validateActivityDates(activities) {
+  let result = true;
+
+  activities.forEach(a => {
+    const date = DateTime.fromJSDate(this.date).toISODate();
+    const start = DateTime.fromJSDate(a.allocatedInterval.start).toISODate();
+    const end = DateTime.fromJSDate(a.allocatedInterval.end).toISODate();
+
+    if (date !== start || date !== end) {
+      result = false;
+    }
+  });
+  return result;
+}
 
 const Schedule = mongoose.model(
   "Schedule",
@@ -18,8 +36,8 @@ const Schedule = mongoose.model(
             type: String,
             required: true
           },
-          allocatedTimeslot: new mongoose.Schema({
-            begining: {
+          allocatedInterval: new mongoose.Schema({
+            start: {
               type: Date,
               required: true
             },
@@ -30,9 +48,45 @@ const Schedule = mongoose.model(
           })
         })
       ],
-      default: []
+      default: [],
+      validate: [
+        validateActivityDates,
+        "Dates of activities do not match the date of the schedule"
+      ]
     }
   })
 );
 
+function validateSchedule(schedule) {
+  const plannedActivitySchema = Joi.object({
+    _id: Joi.objectId(),
+    name: Joi.string()
+      .max(150)
+      .required(),
+    allocatedInterval: Joi.object({
+      _id: Joi.objectId(),
+      start: Joi.date().required(),
+      end: Joi.date()
+        .required()
+        .greater(Joi.ref("start"))
+    }).required()
+  });
+  const schema = Joi.object({
+    _id: Joi.objectId(),
+    date: Joi.date().required(),
+    workingHours: Joi.array()
+      .items(Joi.date())
+      .min(1)
+      .required(),
+    plannedActivities: Joi.array()
+      .items(plannedActivitySchema)
+      .min(1)
+      .required(),
+    __v: Joi.number()
+  });
+
+  return schema.validate(schedule);
+}
+
 exports.Schedule = Schedule;
+exports.validate = validateSchedule;
--- routes/schedules.js
+++ routes/schedules.js
@@ -1,5 +1,5 @@
 const { DateTime } = require("luxon");
-const { Schedule } = require("../models/schedule");
+const { Schedule, validate } = require("../models/schedule");
 const express = require("express");
 const router = express.Router();
 
@@ -10,6 +10,30 @@ router.get("/:date", async (req, res) => {
   }
   const schedule = await Schedule.findOne({ date: date.toJSON() });
   res.send(schedule);
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  if (req.body._id && (await Schedule.findById(req.body._id))) {
+    return res.status(400).send("Schedule already exists");
+  }
+  if (await Schedule.findOne({ date: req.body.date })) {
+    return res.status(400).send("Schedule for a given date already exists");
+  }
+
+  const { date, workingHours, plannedActivities } = req.body;
+  const schedule = new Schedule({ date, workingHours, plannedActivities });
+
+  try {
+    await schedule.save();
+    return res.send(schedule);
+  } catch (ex) {
+    if (ex.name === "ValidationError") {
+      return res.status(400).send(ex.message);
+    }
+    return res.status(500).send(ex.message);
+  }
 });
 
 module.exports = router;
