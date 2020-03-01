1459e0e74a9dddd2eaa9d64a8b1f5bf1b6ed22d8
Ivan Bespalov <slate245@gmail.com>
2020-03-01 13:11:08 +0500

    Added login of existing user

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const schedules = require("../routes/schedules");
 const users = require("../routes/users");
+const auth = require("../routes/auth");
 const error = require("../middleware/error");
 
 module.exports = function(app) {
   app.use(express.json());
   app.use("/api/schedules", schedules);
   app.use("/api/users", users);
+  app.use("/api/auth", auth);
   app.use(error);
 };
