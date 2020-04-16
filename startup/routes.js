035e67e74d0195b481c67d5fb8e3cd113a6bbd2f
Ivan Bespalov <slate245@gmail.com>
2020-04-16 18:56:51 +0500

    Added /activities route

--- startup/routes.js
+++ startup/routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const schedules = require("../routes/schedules");
+const activities = require("../routes/activities");
 const users = require("../routes/users");
 const auth = require("../routes/auth");
 const error = require("../middleware/error");
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(express.json());
   app.use("/api/schedules", schedules);
+  app.use("/api/activities", activities);
   app.use("/api/users", users);
   app.use("/api/auth", auth);
   app.use(error);
