79aad04200d44c8670c6eca89acad2e656ecf720
Ivan Bespalov <slate245@gmail.com>
2020-04-18 17:54:24 +0500

    Added handler for deleting activity

--- routes/activities.js
+++ routes/activities.js
@@ -5,6 +5,16 @@ const router = express.Router();
 
 router.use(auth);
 
+function createActivityFromRequest(req) {
+  const activityObject = { ...req.body };
+  if (activityObject.description === "") {
+    delete activityObject.description;
+  }
+  activityObject.ownerId = req.user._id;
+
+  return activityObject;
+}
+
 router.get("/", async (req, res) => {
   const activities = await Activity.find({ ownerId: req.user._id });
   res.send(activities);
@@ -17,11 +27,7 @@ router.post("/", async (req, res) => {
     return res.status(400).send("Activity already exists");
   }
 
-  const activityObject = { ...req.body };
-  if (activityObject.description === "") {
-    delete activityObject.description;
-  }
-  activityObject.ownerId = req.user._id;
+  const activityObject = createActivityFromRequest(req);
 
   const activity = new Activity(activityObject);
 
@@ -45,10 +51,8 @@ router.put("/:id", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
-  const activityObject = { ...req.body };
-  if (activityObject.description === "") {
-    delete activityObject.description;
-  }
+  const activityObject = createActivityFromRequest(req);
+
   const activity = await Activity.findByIdAndUpdate(
     req.params.id,
     activityObject,
@@ -59,6 +63,20 @@ router.put("/:id", async (req, res) => {
   if (!activity) {
     return res.status(404).send("Activity with a given ID was not found");
   }
+
+  res.send(activity);
+});
+
+router.delete("/:id", async (req, res) => {
+  const activity = await Activity.findById(req.params.id);
+  if (!activity) {
+    return res.status(404).send("Activity with given ID not found");
+  }
+
+  if (activity.ownerId.toString() !== req.user._id) {
+    return res.status(401).send("Access to activities of another user denied.");
+  }
+  await activity.remove();
 
   res.send(activity);
 });
